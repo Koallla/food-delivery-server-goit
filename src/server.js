78ed7f986dcc5fd6cb12bed735647bfddec72998
zmiyov <zmiyov@i.ua>
2020-03-04 17:47:01 +0200

    added route

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const http = require("http");
-const port = 3000;
+const port = 3001;
 
 const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end("Hello Node.js Server!");
+  response.setHeader("Content-Type", "text/html; charset=utf-8;");
+
+  if (request.url === "/home" || request.url === "/") {
+    response.write("<h2>Home</h2>");
+  } else if (request.url == "/about") {
+    response.write("<h2>About</h2>");
+  } else if (request.url == "/contact") {
+    response.write("<h2>Contacts</h2>");
+  } else {
+    response.write("<h2>Not found</h2>");
+  }
+  response.end();
 };
 
 const server = http.createServer(requestHandler);
