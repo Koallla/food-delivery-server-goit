7c338b0d3ab51dbeb8ff76c015f9bf426c2e12f5
Koallla <zmiyov2422@gmail.com>
2020-03-04 22:29:48 +0200

    added nodemon

--- src/server.js
+++ src/server.js
@@ -1,16 +1,40 @@
 const http = require("http");
-const port = 3000;
+const fs = require("fs");
+const path = require("path");
 
 const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end("Hello Node.js Server!");
+  if (request.url === "/home" || request.url === "/") {
+    response.write("<h2>Hello everyone!!!</h2>");
+    return;
+  } else if (request.url == "/products") {
+    const productFilePath = path.join(
+      __dirname,
+      "./db/products/",
+      "all-products.json"
+    );
+    const products = fs.readFileSync(productFilePath);
+
+    response.writeHead(200, {
+      "content-type": "applycation/json"
+    });
+    response.write(products);
+    response.end();
+    return;
+  } else {
+    response.write("<h2>Not found</h2>");
+    response.end();
+  }
 };
 
-const server = http.createServer(requestHandler);
+const startServer = port => {
+  const server = http.createServer(requestHandler);
 
-server.listen(port, err => {
-  if (err) {
-    return console.log("something bad happened", err);
-  }
-  console.log(`server is listening on ${port}`);
-});
+  server.listen(port, err => {
+    if (err) {
+      return console.log("something bad happened", err);
+    }
+    console.log(`server is listening on ${port}`);
+  });
+};
+
+module.exports = startServer;
