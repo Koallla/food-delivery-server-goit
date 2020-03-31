c2a353187332f9a19abf1d21a2449dec3b8d97f5
Koallla <zmiyov2422@gmail.com>
2020-03-31 19:12:45 +0300

    added deleteProduct

--- src/routes/products/deleteProductById.js
+++ src/routes/products/deleteProductById.js
@@ -1,6 +1,6 @@
 const Products = require('../../db/Schemas/products');
 
-const getProductById = (request, response) => {
+const deleteProductById = (request, response) => {
   const id = request.params.id;
 
   const sendResponse = product => {
@@ -14,13 +14,17 @@ const getProductById = (request, response) => {
   const sendError = () => {
     response.status(400);
     response.json({
-      error: 'order was not found',
+      error: 'Product was not found',
     });
   };
 
-  Products.findById(id)
-    .then(sendResponse)
-    .catch(sendError);
+  Products.findOneAndDelete(id)
+    .then(product => {
+      sendResponse(product);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 };
 
-module.exports = getProductById;
+module.exports = deleteProductById;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mainRoute = require('./main/main');
 const addProducts = require('./products/addProduct');
+const getProductById = require('./products/get-productById');
 const getOrderById = require('./orders/getOrderById');
 const createUser = require('./users/signup-users');
 const getUserById = require('./users/get-userById');
 const putUserById = require('./users/putUserById');
 const deleteUserById = require('./users/deleteUserById');
+const deleteProductById = require('./products/deleteProductById');
 const users = require('./users/allUsers');
 const addOrder = require('./orders/addOrder');
 
@@ -14,6 +16,8 @@ const apiRoutes = express.Router();
 apiRoutes
   .get('/', mainRoute)
   .post('/products', addProducts)
+  .get('/products/:id', getProductById)
+  .delete('/products/:id', deleteProductById)
   .get('/orders/:id', getOrderById)
   .get('/users', users)
   .post('/users', createUser)
